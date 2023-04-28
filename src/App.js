4c4025792277014ff275ebd7cd6e5d70d0759a27
sivaji <shivajidaasari@gmail.com>
2023-04-28 10:54:32 +0000

    TextEditor

---
+++ src/App.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-unresolved */
+import './App.css'
+// eslint-disable-next-line import/extensions
+import TextEditor from './components/TextEditor'
+
+const App = () => <TextEditor />
+
+export default App
